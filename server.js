73ba9b7628b455eed61a2b12b23054f3c1542689
Gojo Satrou <satroug36@gmail.com>
2026-09-07 15:57:37 +0530

    add filesystem project

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('I am nishant kumar');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
